36a571d1b772fdbfc33c4551bcd1304d3ec45d41
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2022-11-10 22:57:39 +0300

    ubuntu
    install virtual machine & Ubuntu and run api

---
+++ apiWithUbuntu/acikkaynaktestrepo/restapi/flask_app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        return {'data': data}, 200
+
+    def post(self):
+        name = request.args['name']
+        age = request.args['age']
+        city = request.args['city']
+
+        data = pd.read_csv('users.csv')
+
+        new_data = pd.DataFrame({
+            'name': [name],
+            'age': [age],
+            'city': [city]
+        })
+
+        data = data.append(new_data, ignore_index=True)
+        data.to_csv('users.csv', index=False)
+        return {'data': new_data.to_dict('records')}, 200
+
+    def delete(self):
+        name = request.args['name']
+        data = pd.read_csv('users.csv')
+        data = data[data['name'] != name]
+
+        data.to_csv('users.csv', index=False)
+        return {'message': 'Record deleted successfully.'}, 200
+
+
+class Cities(Resource):
+    def get(self):
+        data = pd.read_csv('users.csv', usecols=[2])
+        data = data.to_dict('records')
+
+        return {'data': data}, 200
+
+
+class Name(Resource):
+    def get(self, name):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        for entry in data:
+            if entry['name'] == name:
+                return {'data': entry}, 200
+        return {'message': 'No entry found with this name !'}, 404
+
+
+# Add URL endpoints
+api.add_resource(Users, '/users')
+api.add_resource(Cities, '/cities')
+api.add_resource(Name, '/<string:name>')
+
+if __name__ == '__main__':
+    #     app.run(host="0.0.0.0", port=5000)
+    app.run()
